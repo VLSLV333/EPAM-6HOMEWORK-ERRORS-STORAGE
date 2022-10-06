2d8caadb130e85f760cd0eeebbbd8cae7ab4dc30
VLSLV333 <v.sherstyk@gmail.com>
2022-10-06 17:46:58 +0000

    created listeners that are present for each page separatly

--- homework/js/index.js
+++ homework/js/index.js
@@ -13,11 +13,38 @@ function viewResults() {
 
 // addEventListener('pageshow', (event) => {console.log("You have visited this page!") });
 
-addEventListener('pageshow', (event) => {
-	for (let i = 0; i < 4; i++){
-		localStorage.setItem(`this is main page ${i}`, `count : ${i}`)
+// addEventListener('pageshow', (event) => {
+// 	for (let i = 0; i < 4; i++){
+// 		localStorage.setItem(`this is main page ${i}`, `count : ${i}`)
+// 	}
+// });
+
+let firstPageVisitsCount = document.getElementById("visit-count1");
+let secondPageVisitsCount = document.getElementById("visit-count2");
+let thirdPageVisitsCount = document.getElementById("visit-count3");
+
+// console.log(secondPageVisitsCount);
+// console.log(firstPageVisitsCount);
+
+
+addEventListener('pageshow', (e) => {
+	if (firstPageVisitsCount != null){
+		firstPageVisitsCount.innerHTML = "Vlad"
+	}
+});
+addEventListener('pageshow', (e) => {
+	if (secondPageVisitsCount != null){
+		// console.log("Vlad")
+		secondPageVisitsCount.innerHTML = "Vlad"
+	}
+	
+});
+addEventListener('pageshow', (e) => {
+	if (thirdPageVisitsCount != null){
+		thirdPageVisitsCount.innerHTML = "Vlad"
 	}
 });
 
-let v = document.querySelector(".active")
-console.log(v)
+
+
+// console.log(v)
