55260a9220d6cb608cc20564a2eb9bd2cdf784a9
VLSLV333 <v.sherstyk@gmail.com>
2022-10-06 13:39:58 +0000

    add listeners to each page and count visits?

--- homework/js/index.js
+++ homework/js/index.js
@@ -5,3 +5,19 @@ function visitLink(path) {
 function viewResults() {
 	//your code goes here
 }
+
+// localStorage.clear();
+
+// let storageSupportedCheck = window.sessionStorage && window.localStorage ? "is" : "is not";
+// console.log(`Storage API ${storageSupportedCheck} supported.`)
+
+// addEventListener('pageshow', (event) => {console.log("You have visited this page!") });
+
+addEventListener('pageshow', (event) => {
+	for (let i = 0; i < 4; i++){
+		localStorage.setItem(`this is main page ${i}`, `count : ${i}`)
+	}
+});
+
+let v = document.querySelector(".active")
+console.log(v)
