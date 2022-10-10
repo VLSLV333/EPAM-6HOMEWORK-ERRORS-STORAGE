5d60354e5b24e1f42f0c6d24d063710f22ef982c
VLSLV333 <v.sherstyk@gmail.com>
2022-10-10 11:17:10 +0000

    everything seems ready. Will commit to gitlab and check once more

--- homework/js/pipe.js
+++ homework/js/pipe.js
@@ -1,21 +1,20 @@
-
-//		Цю функцію ми походу модернізуємо, для того щоб вона перевіряла чи передані функції є дійсно функціями, а якщо ні - має видавати ерор
-
-// function isFunction(functionToCheck) {
-// 	return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
-// }
-
+function isFunction(functionToCheck) {
+	return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+}
 const pipe = (value, ...funcs) => {
-	try{
-		for (let i = 0; i < funcs.length; i++){
-			value = funcs[i](value);
+		try{
+			for (let i = 0; i < funcs.length; i++){
+				if (isFunction(funcs[i])){
+					value = funcs[i](value)
+				} else {
+					throw new SyntaxError('Provided argument at position 2 is not a function!');
+				}
+			}
+			return value;
+		} catch(e){
+			return e.message;
 		}
-		return value;
-	} catch (e){
-		return 'Provided argument at position 2 is not a function!';
-	}
 };
-
 const replaceUnderscoreWithSpace = (value) => value.replace(/_/g, ' ');
 const capitalize = (value) =>
 	value
@@ -24,10 +23,10 @@ const capitalize = (value) =>
 		.join(' ');
 const appendGreeting = (value) => `Hello, ${value}!`;
 
-// const error = pipe('john_doe', replaceUnderscoreWithSpace, capitalize, '');
+const error = pipe('john_doe', replaceUnderscoreWithSpace, capitalize, '');
 
-// alert(error); // Provided argument at position 2 is not a function!
+alert(error);
 
-// const result = pipe('john_doe', replaceUnderscoreWithSpace, capitalize, appendGreeting);
+const result = pipe('john_doe', replaceUnderscoreWithSpace, capitalize, appendGreeting);
 
-// alert(result); // Hello, John Doe!
+alert(result);
